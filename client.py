974c4eddf82f6afff302784af8f7f40ce3217dc9
lewiechiu <louiechiu@gmail.com>
2020-01-17 21:46:49 +0800

    Fixed port issue

--- client.py
+++ client.py
@@ -22,7 +22,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
 def SocketSetup():
 	HOST = '127.0.0.1'  # The server's hostname or IP address
-	PORT = 65432        # The port used by the server
+	PORT = 8000       # The port used by the server
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((HOST, PORT))
 	return s
